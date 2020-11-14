4a7c9156a96f4a94930d59db7a8c2e8c31333724
Yves Loeys <yves.loeys@gmail.com>
2020-11-14 14:06:37 +0100

    Modified server.js and .env

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,29 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
 
 const app = express()
 
 //Import routes from the routes folder => notice  you import from the routes folder de auth file
 const authRoutes = require('./routes/auth')
 
-//apply middleware : ('use') method is part of express
+//Middleware used for app
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+//app.use(cors())  //allows acces from all origin
+if (process.env.NODE_ENV = 'development') {
+    app.use(cors({origin: `http://localhost:3000`}))
+}
+
+
+//Middleware used for routes
 app.use('/api',authRoutes)
 
-const port = process.env.port || 8000
+const port = process.env.PORT || 8000
 
 app.listen(port , () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port} in ${process.env.NODE_ENV} mode`);
 })
