5f78ed089b18bc38a26a87fc84d4fd3a5a5654fb
Yves Loeys <yves.loeys@gmail.com>
2020-11-15 14:29:19 +0100

    Modified gitignore

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,28 +1,35 @@
 const User = require('../models/userModel')
 
-exports.signup = (req,res) => {
-   // console.log('REQ.BODY ON SIGNUP',req.body);
+//signup without email confirmation 
+// exports.signup = (req,res) => {
+//    // console.log('REQ.BODY ON SIGNUP',req.body);
      
-   const {name,email,password} = req.body;
+//    const {name,email,password} = req.body;
 
-   //email here is req.body.email but we destructured up here, so it is User.findOne({email: req.body.email})
-   User.findOne({email}).exec((err,user)=> {
-       if(user){
-           res.status(400).json({error:'Email is already taken'})
-       }
-   }) 
+//    //email here is req.body.email but we destructured up here, so it is User.findOne({email: req.body.email})
+//    User.findOne({email}).exec((err,user)=> {
+//        if(user){
+//            res.status(400).json({error:'Email is already taken'})
+//        }
+//    }) 
    
-   const newUser = new User({name,email,password})
+//    const newUser = new User({name,email,password})
 
-   newUser.save((err,succes)=> {
-      if(err){
-         console.log('SignUp failed',err)
-         return res.status(400).json({
-             error: err
-         })
-      }
-      res.json({
-          message: 'SignUp succesfull , please login'
-      })
-   })
+//    newUser.save((err,succes)=> {
+//       if(err){
+//          console.log('SignUp failed',err)
+//          return res.status(400).json({
+//              error: err
+//          })
+//       }
+//       res.json({
+//           message: 'SignUp succesfull , please login'
+//       })
+//    })
+// }\
+
+
+
+exports.signup = (req,res) => {
+    
 }
