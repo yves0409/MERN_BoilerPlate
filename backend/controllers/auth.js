c5ec4d9956adb5b10e54e5b77cd3bdfc8f3bb7d1
Yves Loeys <yves.loeys@gmail.com>
2020-11-15 14:15:36 +0100

    Register user w/o email confirmation

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,28 @@
+const User = require('../models/userModel')
+
 exports.signup = (req,res) => {
-    res.json({
-        data: 'You hit the signup endpoint from controllers '
-    })
+   // console.log('REQ.BODY ON SIGNUP',req.body);
+     
+   const {name,email,password} = req.body;
+
+   //email here is req.body.email but we destructured up here, so it is User.findOne({email: req.body.email})
+   User.findOne({email}).exec((err,user)=> {
+       if(user){
+           res.status(400).json({error:'Email is already taken'})
+       }
+   }) 
+   
+   const newUser = new User({name,email,password})
+
+   newUser.save((err,succes)=> {
+      if(err){
+         console.log('SignUp failed',err)
+         return res.status(400).json({
+             error: err
+         })
+      }
+      res.json({
+          message: 'SignUp succesfull , please login'
+      })
+   })
 }
