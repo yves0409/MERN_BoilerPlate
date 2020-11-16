580966fae8493dfb171111ff4af24e5f5f8e3683
Yves Loeys <yves.loeys@gmail.com>
2020-11-16 18:18:49 +0100

    Added react router

--- frontend/src/core/Layout.js
+++ frontend/src/core/Layout.js
@@ -1,10 +1,11 @@
 import React,{Fragment} from 'react'
+import {Link } from 'react-router-dom'
 
 const Layout = ({children}) => {
     const nav =()=> (
       <ul className="nav nav-tabs bg-primary">
           <li className="nav-item">
-              <a href="/" className="text-light">Home</a>
+              <Link to="/" className="text-light">Home</Link>
           </li>
       </ul>
     )
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Routes from './Routes';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ <Routes/>
+,  document.getElementById('root')
 );
 
 
