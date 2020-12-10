be8e85b1451ba8e456411f669a32fda3143d5f14
Yves Loeys <yves.loeys@gmail.com>
2020-12-10 11:04:01 +0100

    update admin page information

--- frontend/src/core/Admin.js
+++ frontend/src/core/Admin.js
@@ -1,12 +1,139 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {Link,Redirect} from 'react-router-dom'
+import axios from 'axios'
 import Layout from '../core/Layout'
+import { isAuth,getCookie, signout, updateUser} from '../auth/helpers'
+import {ToastContainer,toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.min.css'
 
-const Admin = () => {
-    return (
+const Admin = ({history}) => {
+  const [values,setValues] = useState({
+      role:'',
+      name:'',
+      email:'',
+      password:'',
+      buttonText:'Submit'
+  })
+
+  const token = getCookie('token')
+
+  useEffect(()=> {
+    loadProfile()
+  },[])
+
+  
+
+  const loadProfile = () => {
+    
+     axios({
+       method:'GET',
+       url:`${process.env.REACT_APP_API}/user/${isAuth()._id}`,
+       headers:  {
+         Authorization: `Bearer ${token}`
+       }
+     })
+     .then(response => {console.log('Profile updated',response)
+     const {role,name,email,password}= response.data;
+     setValues({...values, role,name,email})})
+     .catch(error => {
+      console.log('PRIVATE PROFILE UPDATE ERROR', error.response.data.error);
+      if (error.response.status === 401) {
+          signout(() => {
+              history.push('/');
+          });
+      }
+  });
+};
+
+
+
+  
+  const {role,name,email,password,buttonText} = values
+  
+
+ 
+
+ const  handleChange=(name)=> (event)=> {
+     console.log(event.target.value);
+     setValues({...values,[name]:event.target.value})
+  }
+
+  const handleSubmit = event => {
+    console.log('submitted');
+    event.preventDefault();
+    setValues({...values,buttonText:'Submitting'})
+   
+    axios({
+        method: 'PUT',
+        url:`${process.env.REACT_APP_API}/admin/update`,
+        data: {name,password},
+        headers:  {
+          Authorization: `Bearer ${token}`
+        },
+        data: {name,password},
+      })
+    .then(response => {
+        console.log('Private profile update success',response);
+        updateUser(response, ()=> {
+          console.log('works or not')
+          setValues({...values,buttonText:'Submitted'})
+          toast.success('Profile Updated Succesfully')
+        });
+        
+    })
+  
+    .catch(error => {
+        console.log(error);
+        console.log('Private profile update error',error.response.data);
+        setValues({...values,buttonText:'Submit'})
+        toast.error(error.response.data.error)
+    })
+  }
+
+  const updateForm =()=> (
+      <form>
+      <div className="form-group">
+            <label className="text-muted">Role</label>
+            <input  defaultValue={role} type="text" className="form-control" disabled/>
+        </div>
+
+        <div className="form-group">
+            <label className="text-muted">Name</label>
+            <input onChange={handleChange('name')} value={name} type="text" className="form-control"/>
+        </div>
+
+        <div className="form-group">
+            <label className="text-muted">Email</label>
+            <input  defaultValue={email} type="email" className="form-control" disabled/>
+        </div>
+
+        <div className="form-group">
+            <label className="text-muted">Password</label>
+            <input onChange={handleChange('password')} value={password} type="password" className="form-control"/>
+        </div>
+
+        <div>
+            <button className="btn btn-primary" onClick={handleSubmit}>{buttonText}</button>
+        </div>
+
+      </form>
+  )
+  return (
       <Layout>
-          <h1>Admin Page</h1>
+          {/* {JSON.stringify({name,email,password})} */}
+          <div className="col-md-6 offset-md-3">
+          <ToastContainer/>
+          
+           <h1 className="pt-5 text-center" >Admin Page</h1>
+           <p className="lead text-center">Update Profile</p>
+           {updateForm()}
+
+          </div>
+         
+          
       </Layout>
-    )
+  )
 }
 
 export default Admin
+
