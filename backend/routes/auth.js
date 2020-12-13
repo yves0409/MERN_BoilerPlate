5aab38851ca17d002ee5bbdf9f71cecc9ed1551b
Yves Loeys <yves.loeys@gmail.com>
2020-12-13 21:14:31 +0100

    minor modification

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 
 //Import controllers
-const {signup,accountActivation,signin,forgotPassword,resetPassword} = require('../controllers/auth');
+const {signup,accountActivation,signin,forgotPassword,resetPassword,googleLogin} = require('../controllers/auth');
 
 //import validator
 const {userSignupVal,userSigninVal,forgotPasswordVal,resetPasswordVal} = require('../validators/auth')  //used a middleware and passed in the router
@@ -16,6 +16,8 @@ router.post('/signin',userSigninVal,runValidator, signin)
 //forgot and reset password route
 router.put('/forgot-password', forgotPasswordVal,runValidator,forgotPassword)
 router.put('/reset-password', resetPasswordVal,runValidator,resetPassword)
+//social login
+router.post('/google-login',googleLogin)
 
 
 
